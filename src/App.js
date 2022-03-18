a73b8cb63898bf2569cccc76685f75ad14e9c277
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-03-18 21:05:04 +0600

    use effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
+function ExternalUsers(){
+  const [users , setUsers] = useState([]);
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return(
+    <div>
+      {
+        users.map(user => <ShowUserDetail name = {user.name} Email = {user.email}></ShowUserDetail>)
+      }
+    </div>
+  )
+}
+function ShowUserDetail(props){
+  return(
+    <div style={{border: "2px solid red", padding : "20px", margin : "30px"}}>
+      <h2>Name : {props.name}</h2>
+      <p>Email : {props.Email}</p>
+    </div>
+  )
+}
+
+function Counter(){
+  let [count , setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => {
+    if(count){
+      setCount(count - 1);
+    }
+  };
+  return (
+    <div>
+      <h2>Counter: {count} </h2>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>decrement</button>
+    </div>
+  )
+}
 export default App;
